40589a1e360a69057020d8b4240aaf7758c69763
daina11 <1099500268@qq.com>
2021-04-08 12:58:25 +0800

    '4-8'

---
+++ src/main/java/com/evan/meituan/pojo/User.java
@@ -0,0 +1,60 @@
+package com.evan.meituan.pojo;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
+
+
+@Entity
+@Table(name="user")
+
+//作用是在 json 序列化时忽略 bean 中的一些不需要转化的属性
+@JsonIgnoreProperties({"handler","hibernateLazyInitializer"})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    int id;
+    String username;
+    String password;
+    String email;
+    int role;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getRole() {
+        return role;
+    }
+
+    public void setRole(int role) {
+        this.role = role;
+    }
+
+}
